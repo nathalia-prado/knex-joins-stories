6ebb6c206d442973853a60175662e87871a60d96
Gerard Paapu <gerard@gerardpaapu.com>
2023-07-20 10:26:51 +1200

    finished week 2

--- server/test/routes.test.js
+++ server/test/routes.test.js
@@ -1,11 +1,10 @@
-const request = require('supertest')
-const server = require('../server')
+import { test, expect } from 'vitest'
+import request from 'supertest'
 
-test('list wombles', () => {
+import server from '../server.js'
+
+test('list wombles', async () => {
   const expected = 'WOMBLES!'
-  return request(server)
-    .get('/')
-    .then((res) => {
-      return expect(res.text).toBe(expected)
-    })
+  const res = await request(server).get('/')
+  expect(res.text).toBe(expected)
 })
